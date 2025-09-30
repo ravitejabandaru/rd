31930f9d04da51bf0906d039e5daa2abf2f258ab
Raviteja Bandaru <ravitejabandaru77@gmail.com>
2025-09-30 19:39:55 +0100

    Update testCallRecording.js

--- testCallRecording.js
+++ testCallRecording.js
@@ -1,30 +1,10 @@
-import { LightningElement, wire, track } from 'lwc';
-import getCallRecording from '@salesforce/apex/CallRecordingController.getCallRecording';
+import { LightningElement, api, track } from 'lwc';
 
-export default class CallRecordingPlayer extends LightningElement {
+export default class CallRecording extends LightningElement {
+    @api callId;
     @track audioUrl;
 
     connectedCallback() {
-        this.loadAudio();
-    }
-
-    loadAudio() {
-        getCallRecording({ callId: 'abc-123' })
-            .then(base64Data => {
-                // Convert Base64 to Blob
-                const byteCharacters = atob(base64Data);
-                const byteNumbers = new Array(byteCharacters.length);
-                for (let i = 0; i < byteCharacters.length; i++) {
-                    byteNumbers[i] = byteCharacters.charCodeAt(i);
-                }
-                const byteArray = new Uint8Array(byteNumbers);
-                const blob = new Blob([byteArray], { type: 'audio/wav' });
-
-                // Create an Object URL
-                this.audioUrl = URL.createObjectURL(blob);
-            })
-            .catch(error => {
-                console.error('Error loading audio:', error);
-            });
+        this.audioUrl = `/services/apexrest/CallRecording/${this.callId}`;
     }
 }
