aa4faf64ba256b46bc926a019aaf5420655bb626
Raviteja Bandaru <ravitejabandaru77@gmail.com>
2025-09-30 19:56:25 +0100

    Update testCallRecording.js

--- testCallRecording.js
+++ testCallRecording.js
@@ -1,10 +1,23 @@
 import { LightningElement, api, track } from 'lwc';
 
-export default class CallRecording extends LightningElement {
+export default class CallRecordingPlayer extends LightningElement {
     @api callId;
     @track audioUrl;
 
     connectedCallback() {
-        this.audioUrl = `/services/apexrest/CallRecording/${this.callId}`;
+        this.loadRecording();
+    }
+
+    async loadRecording() {
+        try {
+            const response = await fetch(
+                `/services/apexrest/CallRecording/${this.callId}`, 
+                { headers: { "Range": "bytes=0-" } }
+            );
+            const blob = await response.blob();
+            this.audioUrl = URL.createObjectURL(blob);
+        } catch (e) {
+            console.error("Error fetching recording", e);
+        }
     }
 }
